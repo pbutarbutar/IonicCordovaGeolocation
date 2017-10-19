9b27489d9e5fbd937cf59d1d87207bf684c5464b
Dragan Gaic <dragan.gaic@allianz.at>
2017-10-19 16:39:12 +0200

    DG: The final solution

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController} from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-	constructor(public navCtrl: NavController) {
+	map: any;
+
+	constructor(public navCtrl: NavController, public geolocation: Geolocation) {
 
 	}
+
+	ionViewDidLoad(){
+		this.initializeMap();
+	}
+
+	initializeMap() {
+
+		 this.geolocation.getCurrentPosition().then((position) => {
+
+			let options = {
+			  center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+			  zoom: 16,
+			  mapTypeId: google.maps.MapTypeId.ROADMAP
+			}
+
+			this.map = new google.maps.Map(document.getElementById("map_canvas"), options);
+		});
+	} 
 }
